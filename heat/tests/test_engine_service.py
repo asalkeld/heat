02b2b597eae79eab0c1d7c6bfa1c08d2e1bed3d4
Steven Hardy <shardy@redhat.com>
2014-08-29 16:19:14 +0100

    engine service add stack_user_project_id to create_stack interface
    Adds the option to pass stack_user_project_id to align with the recent
    change which makes nested stacks inherit the stack user project from
    their parent ref bug #1343163.  This will enable the same behavior to
    be maintained when creating stacks via RPC calls instead of directly.
    
    Change-Id: I2a102722d3e5228ccbb71ca13f4246725626f5a9
    blueprint: decouple-nested

--- heat/tests/test_engine_service.py
+++ heat/tests/test_engine_service.py
@@ -446,7 +446,8 @@ class StackServiceCreateUpdateDeleteTest(HeatTestCase):
         environment.Environment(params).AndReturn(stack.env)
         parser.Stack(self.ctx, stack.name,
                      stack.t, stack.env, owner_id=None,
-                     nested_depth=0, user_creds_id=None).AndReturn(stack)
+                     nested_depth=0, user_creds_id=None,
+                     stack_user_project_id=None).AndReturn(stack)
 
         self.m.StubOutWithMock(stack, 'validate')
         stack.validate().AndReturn(None)
@@ -499,7 +500,8 @@ class StackServiceCreateUpdateDeleteTest(HeatTestCase):
                      stack.env,
                      owner_id=None,
                      nested_depth=0,
-                     user_creds_id=None).AndReturn(stack)
+                     user_creds_id=None,
+                     stack_user_project_id=None).AndReturn(stack)
 
         self.m.StubOutWithMock(stack, 'validate')
         stack.validate().AndRaise(exception.StackValidationFailed(
@@ -555,13 +557,15 @@ class StackServiceCreateUpdateDeleteTest(HeatTestCase):
         environment.Environment(params).AndReturn(stack.env)
         parser.Stack(ctx_no_pwd, stack.name,
                      stack.t, stack.env, owner_id=None,
-                     nested_depth=0, user_creds_id=None).AndReturn(stack)
+                     nested_depth=0, user_creds_id=None,
+                     stack_user_project_id=None).AndReturn(stack)
 
         templatem.Template(template, files=None).AndReturn(stack.t)
         environment.Environment(params).AndReturn(stack.env)
         parser.Stack(ctx_no_user, stack.name,
                      stack.t, stack.env, owner_id=None,
-                     nested_depth=0, user_creds_id=None).AndReturn(stack)
+                     nested_depth=0, user_creds_id=None,
+                     stack_user_project_id=None).AndReturn(stack)
 
         self.m.ReplayAll()
 
@@ -609,7 +613,8 @@ class StackServiceCreateUpdateDeleteTest(HeatTestCase):
                      stack.env,
                      owner_id=None,
                      nested_depth=0,
-                     user_creds_id=None).AndReturn(stack)
+                     user_creds_id=None,
+                     stack_user_project_id=None).AndReturn(stack)
 
         self.m.ReplayAll()
 
